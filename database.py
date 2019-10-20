252c9658c1ebed0cbc22c573bf352caa1fdf730b
Kai Qian <32373116+kaiqiann@users.noreply.github.com>
2019-10-19 23:09:23 -0400

    scrap data from hotels.com and can be analysed in sql

--- database.py
+++ database.py
@@ -1,27 +1,80 @@
-#let's do it
+from re import findall,sub
+from lxml import html
+from time import sleep
+from selenium import webdriver
+from pprint import pprint
 import mysql.connector
 
 mydb = mysql.connector.connect(
     host="localhost",
     user='root',
     password='mySQL',
-    database='mysql'
-
+    database='mydatabase'
 )
-mycursor = mydb.cursor()
-#1. need to convert nondigital value into digital value, such as comfortness
-#2. hotel name can be url
-#3. one table for car rental, one table for plane tickets
 
-mycursor.execute("CREATE TABLE website (id INT AUTO_INCREMENT PRIMARY KEY, hotelname VARCHAR(255), price VARCHAR(255), convinience VARCHAR(255), comfortness VARCHAR(255))")
-sql = "INSERT INTO website (hotelname,price,convinience,comfortness) VALUES ('Hilton', '200', 'yes', 'yes')"
-sql = "INSERT INTO website (hotelname,price,convinience,comfortness) VALUES ('Sheraton', '250', 'No', 'No')"
-sql = "INSERT INTO website (hotelname,price,convinience,comfortness) VALUES ('Hampton', '300', 'No', 'yes')"
-sql = "INSERT INTO website (hotelname,price,convinience,comfortness) VALUES ('Hampton', '500', 'yes', 'yes')"
+def parse(url):
+    searchKey = "Boston"  # Change this to your city
+    checkInDate = '10/19'  # Format %d/%m/%Y
+    checkOutDate = '10/20'  # Format %d/%m/%Y
+    response = webdriver.Chrome()
+    response.get(url)
+    searchKeyElement = response.find_elements_by_xpath('//input[contains(@id,"destination")]')
+    checkInElement = response.find_elements_by_xpath('//input[contains(@class,"check-in")]')
+    checkOutElement = response.find_elements_by_xpath('//input[contains(@class,"check-out")]')
+    submitButton = response.find_elements_by_xpath('//button[@type="submit"]')
+    if searchKeyElement and checkInElement and checkOutElement:
+        searchKeyElement[0].send_keys(searchKey)
+        checkInElement[0].clear()
+        checkInElement[0].send_keys(checkInDate)
+        checkOutElement[0].clear()
+        checkOutElement[0].send_keys(checkOutDate)
+        randomClick = response.find_elements_by_xpath('//h1')
+        if randomClick:
+            randomClick[0].click()
+        submitButton[0].click()
+        sleep(15)
+        dropDownButton = response.find_elements_by_xpath('//fieldset[contains(@id,"dropdown")]')
+        if dropDownButton:
+            dropDownButton[0].click()
+            priceLowtoHigh = response.find_elements_by_xpath('//li[contains(text(),"low to high")]')
+            if priceLowtoHigh:
+                priceLowtoHigh[0].click()
+                sleep(10)
 
-mycursor.execute(sql)
-mydb.commit()
-mycursor.execute("SELECT * FROM website ORDER BY hotelname , comfortness ")
+    mycursor = mydb.cursor()
+    mycursor.execute("""truncate table hotels""")#clean table
+    mydb.commit()
+    parser = html.fromstring(response.page_source, response.current_url)
+    hotels = parser.xpath('.//section[@class="hotel-wrap"]')
+    for hotel in hotels[:10]:  # Replace 5 with 1 to just get the cheapest hotel
+        hotelName = hotel.xpath('.//h3[@class="p-name"]/a')
+        hotelName = hotelName[0].text_content() if hotelName else None
+        price = hotel.xpath('.//div[@class="price"]/a//ins')
+        price = price[0].text_content().replace(",", "").strip() if price else None
+        if price == None:
+            price = hotel.xpath('.//div[@class="price"]/a')
+            price = price[0].text_content().replace(",", "").strip() if price else None
+        price = findall('([\d\.]+)', price) if price else None
+        price = price[0] if price else None
+        rating = hotel.xpath('.//div[@class="reviews-box resp-module"]')
+        rating = rating[0].text_content()
+        rating = findall('([\d.]+)', rating)[0]
+        sql = "INSERT INTO hotels (hotelname,price,rating) VALUES (%s,%s,%s)"
+        val = (hotelName,price,rating)
+        mycursor.execute(sql,val)
+        mydb.commit()
+        """item = {
+            "hotelName": hotelName,
+            "price": price,
+            "rating": rating,
+        }
+        pprint(item)"""
+    mycursor.execute("SELECT * FROM hotels ORDER BY rating DESC , price")
+    for x in mycursor.fetchall():
+        print(x)
 
-for x in mycursor.fetchall():
-    print(x)
+
+
+if __name__ == '__main__':
+    parse('http://www.hotels.com')
+
